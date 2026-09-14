b50fb4862c26a8d4d9512db7baa2f68014ac55e8
prsnl-hirai <a.hirai@personal-corp.jp>
2026-09-14 12:16:09 +0900

    環境変数名変更

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export async function POST(req: Request) {
   const supabase = createClient(
-    process.env.SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!,
   );
 
